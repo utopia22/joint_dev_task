8a51d2b214c441034b59ee723cbc74995e4637eb
utopia22 <sekishinya@mba.local>
2020-03-01 10:00:58 +0900

    add q7 to task.rb

--- r_kiso.rb
+++ r_kiso.rb
@@ -11,3 +11,9 @@ names.each.with_index(1) do |name, i|
 puts "会員No.#{i} #{name}さん"
 
 end
+
+
+array = ["1", "2", "3", "4", "5"]
+
+# 以下に回答を記載
+p array.map!(&:to_i)
--- task.rb
+++ task.rb
@@ -58,8 +58,8 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
+  p array.map!(&:to_i)
 
-  p array.map(&:to_i)
 end
 
 def q8
