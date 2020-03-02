65ab3dbd34ae74307112c7d976d7aadc0467e334
utopia22 <sekishinya@mba.local>
2020-03-02 20:58:44 +0900

    add Q8_before to task.rb

--- r_kiso.rb
+++ r_kiso.rb
@@ -22,7 +22,7 @@ p array.map!(&:to_i)
 programming_languages = %w(ruby php python javascript)
 
 # 以下に回答を記載
-programming_languages = programming_languages.collect(&:capitalize)
+programming_languages.collect!(&:capitalize)
 upper_case_programming_languages = programming_languages.map(&:upcase)
 
 # 以下は変更しないで下さい
@@ -33,11 +33,14 @@ p upper_case_programming_languages
 foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
 # 以下に回答を記載
-foods.each do |food|
+#foods.each do |food|
 
-  if food.include?("うに")
-    puts "好物です"
-  else
-    puts "まぁまぁ好きです"
-  end
-end
+  # if food.include?("うに")
+    #puts "好物です"
+  #else
+    #puts "まぁまぁ好きです"
+  #end
+#end
+
+foods.each do |food|
+  food.include?("うに") ? "好物です" : "まぁまぁ好きです"
